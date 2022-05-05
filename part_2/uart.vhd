d0d6f7737da9384866d518b5222099273b376f31
Marek Prochazka <m.prochazka2002@gmail.com>
2022-05-05 20:25:50 +0200

    update

---
+++ part_2/uart.vhd
@@ -0,0 +1,92 @@
+-- Author: Marek Prochazka (xproch0o, https://github.com/marekprochazka)
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity UART_RX is 
+port (
+    CLK:            in std_logic;
+    RST:            in std_logic;
+    DIN:            in std_logic;
+    DOUT:           out std_logic_vector(7 downto 0);
+    DOUT_VLD:     out std_logic
+);
+end UART_RX;
+
+architecture behavioral of UART_RX is
+-- fsm inputs
+signal clk_cnt          : std_logic_vector(4 downto 0):= "00000";
+signal read_finihed     : std_logic := '0';
+
+-- fsm outputs
+signal data_valid       : std_logic := '0';
+signal read_enable      : std_logic := '0';
+signal clk_cnt_reset    : std_logic := '0';
+
+-- another signals 
+signal bit_count        : std_logic_vector(3 downto 0):= "0000";
+begin 
+
+FSM: entity work.UART_FSM(behavioral) 
+    port map (
+        CLK => CLK,
+        RST => RST,
+        DIN => DIN,
+        READ_FINISHED => read_finihed,
+        CLK_CNT => clk_cnt,
+        READ_ENABLE => read_enable,
+        VALID => data_valid,
+        CLK_RST => clk_cnt_reset
+    );
+
+    process(CLK) begin 
+        if rising_edge(CLK) then
+            -- set default valid output to 0
+            DOUT_VLD <= data_valid;
+
+            -- increase counter
+            if clk_cnt_reset = '1' then
+                clk_cnt <= "00000";
+            else 
+                clk_cnt <= clk_cnt + '1';
+            end if;
+
+            -- bit counter 
+            if read_enable = '1' then 
+                bit_count <= bit_count + '1';
+            if data_valid = '1' then 
+                bit_count <= "0000";
+            end if;
+
+            -- read finished comparator
+            if bit_count = "1000" then
+                read_finihed <= '1';
+            else 
+                read_finihed <= '0';
+            end if;
+
+            if data_valid = '1' then 
+                DOUT_VLD <= '1';
+            end if;
+
+            -- reading data
+            case bit_count is 
+                when "0000" => DOUT(0) <= DIN;
+                when "0001" => DOUT(1) <= DIN;
+                when "0010" => DOUT(2) <= DIN;
+                when "0011" => DOUT(3) <= DIN;
+                when "0100" => DOUT(4) <= DIN;
+                when "0101" => DOUT(5) <= DIN;
+                when "0110" => DOUT(6) <= DIN;
+                when "0111" => DOUT(7) <= DIN;
+                when others => null;
+            end case;
+            end if;
+        end if;
+    end process;
+
+
+
+end behavioral;
--- part_2/uart_fsm.vhd
+++ part_2/uart_fsm.vhd
@@ -32,7 +32,7 @@ begin
         elsif (rising_edge(clk)) then
         -- Synchronous determination of the next state 
         -- based on the current state and inputs
-            case state is
+            case state_sig is
                 when IDLE_STATE =>
                     if DIN = '0' then
                         state_sig <= WAIT_READ_STATE;
@@ -40,7 +40,7 @@ begin
                         state_sig <= IDLE_STATE;
                     end if;
                 when WAIT_READ_STATE =>
-                    if CLK_CNT = '10000' then
+                    if CLK_CNT = "10000" then
                         state_sig <= READ_STATE;
                     else 
                         state_sig <= WAIT_READ_STATE;
@@ -52,7 +52,7 @@ begin
                         state_sig <= READ_STATE;
                     end if;
                 when WAIT_END_STATE =>
-                    if CLK_CNT = '10000' then
+                    if CLK_CNT = "10000" then
                         state_sig <= IDLE_STATE;
                     else 
                         state_sig <= WAIT_END_STATE;
@@ -64,8 +64,8 @@ begin
     -- Set output based on current state and inputs
     -- waiting for edge is not necessary
 
-    process(state, DIN) begin
-        case state is
+    process(state_sig, DIN, READ_FINISHED, CLK_CNT) begin
+        case state_sig is
             when IDLE_STATE => 
                 if DIN = '1' then
                     READ_ENABLE <= '0';
@@ -77,7 +77,7 @@ begin
                     CLK_RST <= '1';
                 end if;
             when WAIT_READ_STATE =>
-                if CLK_CNT = '10000' then
+                if CLK_CNT = "10000" then
                     READ_ENABLE <= '0';
                     VALID <= '0';
                     CLK_RST <= '1';
@@ -92,11 +92,11 @@ begin
                     VALID <= '0';
                     CLK_RST <= '1';
                 else    
-                    if CLK_CNT = '01000' then
+                    if CLK_CNT = "01000" then
                         READ_ENABLE <= '1';
                         VALID <= '0';
                         CLK_RST <= '0';
-                    elsif CLK_CNT = '10000' then
+                    elsif CLK_CNT = "10000" then
                         READ_ENABLE <= '0';
                         VALID <= '0';
                         CLK_RST <= '1';
@@ -107,7 +107,7 @@ begin
                     end if;
                 end if;
             when WAIT_END_STATE =>
-                if CLK_CNT = '10000' then
+                if CLK_CNT = "10000" then
                     READ_ENABLE <= '0';
                     VALID <= '1';
                     CLK_RST <= '0';
